52e5b515c231fae5e5804b9b2bced4c7596870a3
Andrew Mill <53652789+Huggy444@users.noreply.github.com>
2021-06-15 01:46:56 +0100

    Removed reference to resources py

--- labourers.py
+++ labourers.py
@@ -4,7 +4,6 @@ import ujson
 import datetime
 from flask import Flask, redirect, url_for, render_template, request, jsonify, make_response
 import time
-from resources import tier_list,lab_list,base_materials,base_loot_amounts,happ_dict,laborer_outputs,t2_tier_list
         
 app = Flask(__name__)
 
